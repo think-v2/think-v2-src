7362832e301d4baa6305537ffdbd33e967896b30
think-v2 <jaceareff@gmail.com>
2025-10-04 12:40:41 -0700

    lyric test fix mobile input

--- components/lyric_test/LyricLine.tsx
+++ components/lyric_test/LyricLine.tsx
@@ -56,9 +56,9 @@ const LyricLine = ({
         className="text-3xl absolute bottom-0 left-1/2 -translate-x-1/2 
         bg-gray-900/50 outline-amber-50/60 outline-2 rounded-xl
         opacity-0 group-hover:opacity-100
-        translate-y-2 group-hover:translate-y-0
-        rotate-x-90 group-hover:rotate-x-0
-        blur-sm group-hover:blur-none
+        translate-y-2 rotate-x-90 blur-sm
+        group-hover:translate-y-0 group-hover:rotate-x-0 group-hover:blur-none
+        pointer-coarse:translate-y-0 pointer-coarse:rotate-x-0 pointer-coarse:blur-none
         transition-all"
       >
         <button onClick={() => updateLineScore(0)} className="p-px border-0">
