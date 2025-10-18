27a6fd9c39fcb9798e2bd3d569f0be30a3fd5697
think-v2 <jaceareff@gmail.com>
2025-10-18 03:06:10 -0700

    PickupParty remove tails

--- components/pickup_party/MessageBubble.tsx
+++ components/pickup_party/MessageBubble.tsx
@@ -23,7 +23,6 @@ const MessageBubble = ({
     <>
       {!sentStyle && (
         <div className="bg-gray-300 rounded-3xl p-4 text-black relative w-fit text-4xl">
-          <div className="z-20 h-1/4 aspect-square bg-gray-300 [clip-path:polygon(100%_100%,100%_0,0_100%)] absolute left-0 bottom-0" />
           {children}
         </div>
       )}
@@ -31,7 +30,6 @@ const MessageBubble = ({
         <div key={messagerId}>
           <div className="w-full flex justify-end">
             <div className="relative w-fit">
-              <div className="z-20 h-1/4 aspect-square bg-blue-600 [clip-path:polygon(100%_100%,0_100%,0_0)] absolute right-0 bottom-0" />
               {hearts != null && hearts > 0 && (
                 <div className="bg-gray-300 rounded-3xl absolute left-0 -top-6 text-black p-1">
                   {hearts != null && "❤️".repeat(Math.min(3, hearts))}
